4f484c940e81b7342266bf73736ad7dd34d564c2
BassV <khorotyanvahagn@gmail.com>
2016-03-16 09:30:47 +0400

    Added files via upload

---
+++ CW Multiples of 3 and 5 (6 kyu)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CW_CSharp_Multiples_of_3_and_5__6_kyu_
+{
+    class Program
+    {     
+        public static int Solution(int value)
+        {
+            List<int> l = new List<int>();
+            int sum = 0;
+
+            for (int i = 0; i < value; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    l.Add(i);
+            }
+
+            foreach (int j in l)
+                sum += j;
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.Solution(10));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CW Multiples of 3 and 5 (6 kyu)/ReadMe !.txt
@@ -0,0 +1,15 @@
+This my solution to the problem below
+http://www.codewars.com/kata/multiples-of-3-and-5
+
+Description:
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+Courtesy of ProjectEuler.net
+
+Examples:
+
+For numbers below 10, the sum is: 23
+For numbers below 0, the sum is: 0
+For numbers below 20, the sum is: 78
+For numbers below 27, the sum is: 168
