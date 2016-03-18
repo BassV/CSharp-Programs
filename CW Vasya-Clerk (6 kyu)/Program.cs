9ba1a8e22eae44b4acb3f224ad1e2dc2315e632d
BassV <khorotyanvahagn@gmail.com>
2016-03-18 12:53:00 +0400

    Added files via upload

---
+++ CW Vasya-Clerk (6 kyu)/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_CSharp_Vasya_Clerk__6_kyu_
+{
+    class Program
+    {
+        public static string Tickets(int[] peopleInLine)
+        {
+            int d25 = 0;
+            int d50 = 0;
+
+            for (int i = 0; i < peopleInLine.Length; i++)
+            {
+                if (peopleInLine[i] == 25)
+                    d25++;
+
+                if (peopleInLine[i] == 50)
+                {
+
+                    d50++;
+                    d25--;
+
+                    if (d25 < 0)
+                        return "NO";
+                }
+                if (peopleInLine[i] == 100)
+                {
+                    while (peopleInLine[i] > 25)
+                    {
+                        if (d50 > 0 && peopleInLine[i] >= 75)
+                        {
+                            peopleInLine[i] -= 50;
+                            d50--;
+                        }
+                        else
+                        {
+                            peopleInLine[i] -= 25;
+                            d25--;
+                        }
+                    }
+                    if (d25 < 0)
+                        return "NO";
+                }
+            }
+            return "YES";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.Tickets(new int[] { }));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CW Vasya-Clerk (6 kyu)/ReadMe !.txt
@@ -0,0 +1,21 @@
+This my solution to the problem below
+http://www.codewars.com/kata/555615a77ebc7c2c8a0000b8
+
+Description:
+
+The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. A "Avengers" ticket costs 25 dollars.
+
+Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+
+Can Vasya sell a ticket to each person and give the change if he initially has no money and sells the tickets strictly in the order people follow in the line?
+
+Return YES, if Vasya can sell a ticket to each person and give the change. Otherwise return NO.
+
+Examples:
+
+// === C Sharp ===
+
+Line.Tickets(new int[] {25, 25, 50}) // => YES
+ Line.Tickets(new int[] {25, 50, 100, 25, 50}) // => YES
+Line.Tickets(new int[] {25, 100})  
+           // => NO. Vasya will not have enough money to give change to 100 dollars
